2cfebbeff265484700902fb54078afbcc7a54ea7
xBananaKing79x <khakimov.t@gmail.com>
2025-06-08 19:46:12 +0300

    Spring Web Adaptation

---
+++ src/main/java/org/skypro/skyshop/model/controller/ShopController.java
@@ -0,0 +1,41 @@
+package org.skypro.skyshop.model.controller;
+
+import org.skypro.skyshop.model.article.Article;
+import org.skypro.skyshop.model.product.Product;
+import org.skypro.skyshop.model.searchengine.SearchResult;
+import org.skypro.skyshop.model.service.SearchService;
+import org.skypro.skyshop.model.service.StorageService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController // Аннотация для REST-контроллера
+public class ShopController {
+
+    private final StorageService storageService; // Внедрение сервиса через конструктор
+    private final SearchService searchService;
+
+    public ShopController(StorageService storageService, SearchService searchService) {
+        this.storageService = storageService;
+        this.searchService = searchService;
+    }
+
+    // Метод для получения всех продуктов
+    @GetMapping("/products")
+    public Collection<Product> getAllProducts() {
+        return storageService.getAllProducts();
+    }
+
+    // Метод для получения всех статей
+    @GetMapping("/articles")
+    public Collection<Article> getAllArticles() {
+        return storageService.getAllArticles();
+    }
+
+    @GetMapping("/search")
+    public Collection<SearchResult> search(@RequestParam String pattern) {
+        return searchService.search(pattern);
+    }
+}
