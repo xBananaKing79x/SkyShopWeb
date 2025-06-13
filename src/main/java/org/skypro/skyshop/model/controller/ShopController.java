6810cd54b7a1576a59c22310f62262ba42df8a7a
xBananaKing79x <khakimov.t@gmail.com>
2025-06-13 15:57:44 +0300

    Класс корзины доработан для отображения объектов корзины отдельного пользователя

--- src/main/java/org/skypro/skyshop/model/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop/model/controller/ShopController.java
@@ -1,25 +1,44 @@
 package org.skypro.skyshop.model.controller;
 
 import org.skypro.skyshop.model.article.Article;
+import org.skypro.skyshop.model.basket.UserBasket;
 import org.skypro.skyshop.model.product.Product;
 import org.skypro.skyshop.model.searchengine.SearchResult;
+import org.skypro.skyshop.model.service.BasketService;
 import org.skypro.skyshop.model.service.SearchService;
 import org.skypro.skyshop.model.service.StorageService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.UUID;
 
 @RestController // Аннотация для REST-контроллера
 public class ShopController {
 
     private final StorageService storageService; // Внедрение сервиса через конструктор
     private final SearchService searchService;
+    private final BasketService basketService;
 
-    public ShopController(StorageService storageService, SearchService searchService) {
+    public ShopController(StorageService storageService, SearchService searchService, BasketService basketService) {
         this.storageService = storageService;
         this.searchService = searchService;
+        this.basketService = basketService;
+    }
+
+    //Меитод получения корзины пользователя
+    @GetMapping("/basket")
+    public UserBasket getUserBasket() {
+        return basketService.getUserBasket();
+    }
+
+    // Метод добавления продукта в корзину
+    @GetMapping("/{id}")
+    public String addProduct(@PathVariable("id") UUID id) {
+        basketService.addProductToBasket(id);
+        return "Продукт успешно добавлен";
     }
 
     // Метод для получения всех продуктов
